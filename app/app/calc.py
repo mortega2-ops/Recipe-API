41d5420b13dd828c2f485b8513330d944c665091
mortega2-ops <mortega2@catalyte.io>
2021-08-16 15:41:19 -0600

    implemented some unit test for users

---
+++ app/app/calc.py
@@ -0,0 +1,32 @@
+
+
+def add(x, y):
+    """A simple function that returns the sum of two integers
+    Parameters
+    ----------
+    x : int, required
+        The first number to be summed
+    y : int, required
+        The second number to be summed
+    Returns
+    -------
+    int
+        a sum of the numbers passed in
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """A simple function that returns the difference of two integers
+    Parameters
+    ----------
+    x : int, required
+        The number to be subtracted
+    y : int, required
+        The number to be subtracted from
+    Returns
+    -------
+    int
+        a difference of the numbers passed in
+    """
+    return y - x
